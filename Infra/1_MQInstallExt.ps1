b8e46c66ea21c6430bfcbd6013722c195a24b525
Mahesh Kshirsagar <maksh@microsoft.com>
2021-03-31 19:55:18 +0100

    added tls

--- Infra/1_MQInstallExt.ps1
+++ Infra/1_MQInstallExt.ps1
@@ -1,11 +1,14 @@
 # Create Install Media Folder
-New-Item -Path "c:\" -Name "MQInstallMedia" -ItemType "directory" -Force
+New-Item -Path "C:\" -Name "MQInstallMedia" -ItemType "directory" -Force
 
 # MQ Installation Path as Of Mar-21
 $source = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqadv/mqadv_dev921_windows.zip'
 
 # Destination to save the file
-$destination = 'c:\MQInstallMedia\mqadv_dev921_windows.zip'
+$destination = 'C:\MQInstallMedia\mqadv_dev921_windows.zip'
+
+# Set TLS
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 
 #Download the file
 Invoke-WebRequest -Uri $source -OutFile $destination
