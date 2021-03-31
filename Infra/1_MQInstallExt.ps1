ba2bb79a1056163685da84cd6b1bdc1ef61d396e
Mahesh Kshirsagar <maksh@microsoft.com>
2021-03-31 18:53:53 +0100

    added download path

--- Infra/1_MQInstallExt.ps1
+++ Infra/1_MQInstallExt.ps1
@@ -1,1 +1,11 @@
-New-Item -Path "c:\" -Name "MQInstallMedia" -ItemType "directory"
+# Create Install Media Folder
+New-Item -Path "c:\" -Name "MQInstallMedia" -ItemType "directory" -Force
+
+# MQ Installation Path as Of Mar-21
+$source = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqadv/mqadv_dev921_windows.zip'
+
+# Destination to save the file
+$destination = 'c:\MQInstallMedia\mqadv_dev921_windows.zip'
+
+#Download the file
+Invoke-WebRequest -Uri $source -OutFile $destination
