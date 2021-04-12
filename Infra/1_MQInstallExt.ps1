786853f2763faf56d0905a371836e2843511574c
Mahesh Kshirsagar <maksh@microsoft.com>
2021-04-12 19:30:48 +0100

    Corrected The Script

--- Infra/1_MQInstallExt.ps1
+++ Infra/1_MQInstallExt.ps1
@@ -1,14 +1,53 @@
-# Create Install Media Folder
-New-Item -Path "C:\" -Name "MQInstallMedia" -ItemType "directory" -Force
-
+# Install Media Folder Exists
+$installFolder = 'C:\Windows\MQInstallMedia'
 # MQ Installation Path as Of Mar-21
-$source = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqadv/mqadv_dev921_windows.zip'
+$installSource = 'https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqadv/mqadv_dev921_windows.zip'
+# MQ Extract FOlder
+$mqExtractFolder = 'C:\MQInstallMedia\mqadv_dev921_windows'
+# MQ Installable MSI Path
+$mqMSIFile = 'C:\MQInstallMedia\mqadv_dev921_windows\MQServer\MSI\IBM MQ.msi'
+# MQ Install Log File Path
+$mqInstallLogFilePath = 'C:\MQInstallMedia\mqadv_dev921_windows\Install.Log'
+
+
+if (!(Test-Path -Path $InstallFolder))
+{
+    # Create Install Media Folder
+    New-Item -Path "C:\" -Name "MQInstallMedia" -ItemType "directory" -Force
+}
 
 # Destination to save the file
-$destination = 'C:\MQInstallMedia\mqadv_dev921_windows.zip'
+$destinationFilePath = 'C:\MQInstallMedia\mqadv_dev921_windows.zip'
 
-# Set TLS
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+if (!(Test-Path -Path $destinationFilePath -PathType Leaf))
+{
+    # Set TLS
+    [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
 
-#Download the file
-Invoke-WebRequest -Uri $source -OutFile $destination
+    #Download the file
+    $response = Invoke-WebRequest -Uri $installSource -OutFile $destinationFilePath
+
+    # If download is successful
+    if ($response.StatusCode -eq 200)
+    {
+        Expand-Archive -LiteralPath $destinationFilePath $mqExtractFolder -Force
+    }
+}
+else {
+    # Check if Zip File in not extracted
+    if (!(Test-Path -Path $mqExtractFolder))
+    {
+        Expand-Archive -LiteralPath $destinationFilePath -DestinationPath $mqExtractFolder -Force
+    }
+
+    # Run MSI
+    if ((Test-Path -Path $mqMSIFile -PathType Leaf))
+    {
+        # Run MSI
+        Start-Process /wait msiexec /i $mqMSIFile /l*v $mqInstallLogFilePath /q RESPONSE="C:\MQInstallMedia\mqadv_dev921_windows\MQServer\Response.ini" TRANSFORMS="1033.mst" AGREETOLICENSE="yes" ADDLOCAL="Server"
+
+        # Check Error Level
+        Write-Output %ERRORLEVEL%
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # messaging
 
 [![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FMahesh-MSFT%2Fmessaging%2Fmain%2FInfra%2Fazuredeploy.json)
+
+
+https://developer.ibm.com/tutorials/mq-connect-app-queue-manager-windows/
+
+https://www.ibm.com/docs/en/ibm-mq/7.5?topic=iwmcw-advanced-installation-using-msiexec
+https://www.ibm.com/docs/en/ibm-mq/9.0?topic=managers-create-multi-instance-queue-manager
